4472f78259a27b96c7d455439aa8604b0128b1e6
kwill775 <spn775@yahoo.com>
2024-09-19 17:46:37 -0700

    updated index.html and index.js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 function showTime(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      let parisTime = moment()
-        .tz("Europe/Paris")
-        .format("dddd, MMMM D, YYYY hh:mm A");
-      alert(`It is ${parisTime} in Europe/Paris`);
-    }
-    if (event.target.value === "tokyo") {
-      let tokyoTime = moment()
-        .tz("Asia/Tokyo")
-        .format("dddd, MMMM D, YYYY hh:mm A");
-      alert(`It is ${tokyoTime} in Asia/Tokyo`);
-    }
-    if (event.target.value === "sydney") {
-      let sydneyTime = moment()
-        .tz("Australia/Sydney")
-        .format("dddd, MMMM D, YYYY hh:mm A");
-      alert(`It is ${sydneyTime} in Australia/Sydney`);
-    }
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY hh:mm A");
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
